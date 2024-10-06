f2e3d096a1ceaaa6e1393ce2c2f3d98a6a3bb73f
Lost-Fly <karamyshevvlad99@gmail.com>
2024-10-06 13:33:11 +0500

    hello world

--- quick-api/src/main/kotlin/com/lostfly/Application.kt
+++ quick-api/src/main/kotlin/com/lostfly/Application.kt
@@ -6,6 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
+    println("APP STARTED")
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
